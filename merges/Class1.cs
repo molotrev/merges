2a2530c827ad210fded121d7760e4c24b3a0ba3b
gernotroth <gernotroth@hotmail.com>
2022-10-27 19:01:19 +0200

    add file

---
+++ merges/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace merges
+{
+    public class Class1
+    {
+        public string Lorem()
+        {
+            return "Lorem Ipsum";
+        }
+    }
+}
